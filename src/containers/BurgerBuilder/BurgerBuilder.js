626de49b87711c5e3076feb95327fff68d4e6b78
Cynoc <cbb67@cornell.edu>
2018-08-11 23:48:38 -0500

    added firebase database

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-
+import axios from '../../axios-order';
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -14,10 +14,6 @@ const INGREDIENT_PRICES = {
 };
 
 class BurgerBuilder extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {...}
-    // }
     state = {
         ingredients: {
             salad: 0,
@@ -80,7 +76,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () =>{
-        alert("Please Continue !");
+        
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer:{
+                name:'Cynoc Bediako',
+                address:{
+                    street: 'TestStreet 1',
+                    zipCode: '14853',
+                    country: 'Ghana'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+            }
+        axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
     }
     render () {
         const disabledInfo = {
